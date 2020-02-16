9f85461f3ec4e1099d0decf876ae29eb6402e601
mstanka <mice78.mm@gmail.com>
2020-02-16 21:04:19 +0100

    init draft layout

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(171, 66%, 44%)",
+        "strong-cyan-dark": "hsl(171, 66%, 33%)",
+        "light-blue": "hsl(233, 100%, 69%)",
+        "light-blue-dark": "hsl(233, 100%, 59%)",
+        "dark-grayish-blue": "hsl(210, 10%, 33%)",
+        "grayish-blue": "hsl(201, 11%, 66%)"
+      },
+      fontFamily: {
+        sans: ['"Bai Jamjuree"', "sans-serif"]
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
