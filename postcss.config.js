9e89c14a7ee0734a9b8dc9462be1032517085af1
mstanka <mice78.mm@gmail.com>
2020-03-22 16:37:25 +0100

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,30 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  // Specify the paths to all of the template files in your project
-  content: [
-    "./docs/index.html",    
-  ],
+// const purgecss = require("@fullhuman/postcss-purgecss")({
+//   // Specify the paths to all of the template files in your project
+//   content: [
+//     "./docs/index.html"
+//   ],
 
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
+//   // Include any special characters you're using in this regular expression
+//   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+// });
+  
+// module.exports = {
+//   plugins: [
+//     require("tailwindcss"),
+//     require("autoprefixer"),
+//     ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+//   ]
+// };
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        './docs/index.html'
+      ],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
   ]
-};
+}
